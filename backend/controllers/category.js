900c9dd8eaab48c4dd523aabfdf419bb10d3142b
Steve Chalco <stevechalco@Steves-MacBook-Pro.local>
2021-06-15 22:05:20 -0700

    error handling

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require("../models/category");
 const slugify = require("slugify");
+const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
   const { name } = req.body;
@@ -8,7 +9,7 @@ exports.create = (req, res) => {
   let category = new Category({ name, slug });
   category.save((err, data) => {
     if (err) {
-      return res.status(400).json({ error: err });
+      return res.status(400).json({ error: errorHandler(err) });
     }
     res.json(data);
   });
