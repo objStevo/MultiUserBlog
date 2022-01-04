f7edbc0732a9b203eb263188386a85123c136371
Steve Chalco <hschalco@gmail.com>
2022-01-04 07:14:25 -0800

    Create list all blogs and categories and tags api

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -3,7 +3,7 @@ const Category = require("../models/category");
 const Tag = require("../models/tag");
 const formidable = require("formidable");
 const slugify = require("slugify");
-const { stripHtml } = require('string-strip-html');
+const { stripHtml } = require("string-strip-html");
 const _ = require("lodash");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 const fs = require("fs");
@@ -106,22 +106,66 @@ exports.create = (req, res) => {
 
 exports.list = (req, res) => {
   Blog.find({})
-      .populate('categories', '_id name slug')
-      .populate('tags', '_id name slug')
-      .populate('postedBy', '_id name username')
-      .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
-      .exec((err, data) => {
-          if (err) {
-              return res.json({
-                  error: errorHandler(err)
-              });
-          }
-          res.json(data);
-      });
+    .populate("categories", "_id name slug")
+    .populate("tags", "_id name slug")
+    .populate("postedBy", "_id name username")
+    .select(
+      "_id title slug excerpt categories tags postedBy createdAt updatedAt"
+    )
+    .exec((err, data) => {
+      if (err) {
+        return res.json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(data);
+    });
 };
 
 exports.listAllBlogsCategoriesTags = (req, res) => {
-  //
+  let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+  let blogs, categories, tags;
+
+  Blog.find({})
+    .populate("categories", "_id name slug")
+    .populate("tags", "_id name slug")
+    .populate("postedBy", "_id name username profile")
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .select(
+      "_id title slug excerpt categories tags postedBy createdAt updatedAt"
+    )
+    .exec((err, data) => {
+      if (err) {
+        return res.json({
+          error: errorHandler(err),
+        });
+      }
+      blogs = data; // blogs
+      // get all categories
+      Category.find({}).exec((err, c) => {
+        if (err) {
+          return res.json({
+            error: errorHandler(err),
+          });
+        }
+        categories = c; // categories
+        // get all tags
+        Tag.find({}).exec((err, t) => {
+          if (err) {
+            return res.json({
+              error: errorHandler(err),
+            });
+          }
+          tags = t;
+          // return all blogs categories tags
+          res.json({ blogs, categories, tags, size: blogs.length });
+        });
+      });
+    });
 };
 
 exports.read = (req, res) => {
