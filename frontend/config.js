ba47dd8d69754bd6f3aeedf30bdfed6368bb3fe8
Steve Chalco <hschalco@gmail.com>
2022-01-11 07:50:18 -0800

    Add SEO head

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,12 @@
-import getConfig from 'next/config'
-const {publicRuntimeConfig} = getConfig()
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://seoblog.com' : 'http://localhost:8000';
+export const API = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.API_PRODUCTION
+    : publicRuntimeConfig.API_DEVELOPMENT;
+
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.DOMAIN_PRODUCTION
+    : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
