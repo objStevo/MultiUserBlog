f36342626d8f0819a85d5376fef39326e9e891a4
Steve Chalco <hschalco@gmail.com>
2022-01-25 07:22:18 -0800

    Handle token expired response

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,6 +1,21 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
 import cookie from "js-cookie";
+import Router from 'next/router';
+
+export const handleResponse = response => {
+  if (response.status === 401) {
+      signout(() => {
+          Router.push({
+              pathname: '/signin',
+              query: {
+                  message: 'Your session is expired. Please signin'
+              }
+          });
+      });
+  }
+  return ({error: 'Token expired'});
+};
 
 export const signup = (user) => {
   return fetch(`${API}/api/signup`, {
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,23 +1,33 @@
 import Layout from "../components/Layout";
 import SigninComponent from "../components/auth/SigninComponent";
-import styled from "styled-components";
+import { withRouter } from 'next/router';
 
-const StyledContainer = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-`;
+const Signin = ({ router }) => {
+  const showRedirectMessage = () => {
+      if (router.query.message) {
+          return <div className="alert alert-danger">{router.query.message}</div>;
+      } else {
+          return;
+      }
+  };
 
-const Signin = () => {
   return (
-    <StyledContainer>
       <Layout>
-        <SigninComponent />
+          <div className="container-fluid">
+              <h2 className="text-center pt-4 pb-4">Signin</h2>
+
+              <div className="row">
+                  <div className="col-md-6 offset-md-3">{showRedirectMessage()}</div>
+              </div>
+
+              <div className="row">
+                  <div className="col-md-6 offset-md-3">
+                      <SigninComponent />
+                  </div>
+              </div>
+          </div>
       </Layout>
-    </StyledContainer>
   );
 };
 
-export default Signin;
+export default withRouter(Signin);
