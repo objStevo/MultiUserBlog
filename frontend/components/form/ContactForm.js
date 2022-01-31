212dfd87608c0f79b7060caa601f12c8cd956db0
Steve Chalco <hschalco@gmail.com>
2022-01-30 21:13:17 -0800

    Create contact form action and component

---
+++ frontend/components/form/ContactForm.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { emailContactForm } from '../../actions/form';
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <React.Fragment>
+            <p>show contact form</p>
+        </React.Fragment>
+    );
+};
+
+export default ContactForm;
