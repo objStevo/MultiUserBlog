2fae5c60ddda66438ce854a558e003106863a29a
Steve Chalco <stevechalco@Steves-MacBook-Pro.local>
2022-02-05 21:41:21 -0800

    MUI Navbar

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import MUIHeader from "./MUIHeader"
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <Header />
+      {/* <Header /> */}
+      <MUIHeader/>
       {children}
       <Footer />
     </React.Fragment>
